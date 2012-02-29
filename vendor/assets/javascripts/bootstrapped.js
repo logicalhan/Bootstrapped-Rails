e3df6701ed654832e01a3c3cee2fccc8f10cf180
Han <hkfortytwo@gmail.com>
2012-02-28 18:07:49 -0800

    fixed js loading probs

--- vendor/assets/javascripts/bootstrapped.js
+++ vendor/assets/javascripts/bootstrapped.js
@@ -1,2 +1,14 @@
-//= require_self
-//= require_directory ./bootstrapped
+//= require bootstrapped/bootstrap-transition
+//= require bootstrapped/bootstrap-button
+//= require bootstrapped/bootstrap-modal
+//= require bootstrapped/bootstrap-scrollspy
+//= require bootstrapped/bootstrap-dropdown
+//= require bootstrapped/bootstrap-alert
+//= require bootstrapped/bootstrap-collapse
+//= require bootstrapped/bootstrap-tab
+//= require bootstrapped/bootstrap-tooltip
+//= require bootstrapped/bootstrap-popover
+//= require bootstrapped/bootstrap-typeahead
+
+
+
